Modify the f-score in problem.py - correct mistake in the count of detected steps - f-score is computed per signal then averaged

problem.py
@@ -143,82 +143,78 @@ def _check_step_list(step_list):
         assert start < end, f"start should be before end: {step}."
 
 
-def count_detected(step_list_1, step_list_2):
-    """A detected step is counted as correct if the mean of its start and end times lies
-    inside an annotated step. An annotated step can only be detected one time.If several
-    detected steps correspond to the same annotated step, all but one are considered as false.
-    The precision is the number of correctly detected steps divided by the total number of
-    detected steps.
+def _step_detection_precision(step_list_true, step_list_pred):
+    """Precision is the number of correctly predicted steps divided by the number of predicted
+    steps. A predicted step is counted as correct if its mid-index (mean of its start and end
+    indexes) lies inside an annotated step.
+    Note that an annotated step can only be detected once. If several predicted steps correspond
+    to the same annotated step, all but one are considered as false.
+    Here, precision is computed on a single prediction task (all steps correspond to the same
+    signal).
 
     The lists y_true_ and y_pred are lists of steps, for instance:
-        - step_list_1: [[357, 431], [502, 569], [633, 715], [778, 849], [907, 989]]
-        - step_list_2: [[293, 365], [422, 508], [565, 642], [701, 789]]
-
-
-    Arguments:
-        step_list_2 {List} -- steps list 1
-        step_list_2 {List} -- step list 2
-
-    Returns:
-        int -- number of detected steps in the first list
-    """
-    _check_step_list(step_list_1)
-    _check_step_list(step_list_2)
-
-    detected_index_set = set()
-    n_detected = 0
-    for (start, end) in step_list_2:
-        mid = (start + end) // 2
-        for (index, (start_true, end_true)) in enumerate(step_list_1):
-            if (index not in detected_index_set) and (start_true <= mid < end_true):
-                n_detected += 1
-                detected_index_set.add(index)
-                break
-
-    return n_detected
-
-
-def _step_detection_precision(y_true, y_pred):
-    """A detected step is counted as correct if the mean of its start and end times lies
-        inside an annotated step.
+        - step_list_true: [[357, 431], [502, 569], [633, 715], [778, 849], [907, 989]]
+        - step_list_pred: [[293, 365], [422, 508], [565, 642], [701, 789]]
 
     Arguments:
-        y_true {List} -- list of true list of steps
-        y_pred {List} -- predicted steps
+        step_list_true {List} -- list of true steps
+        step_list_pred {List} -- list of predicted steps
 
     Returns:
         float -- precision, between 0.0 and 1.0
-
     """
-    n_detected = 0
-    n_steps_pred = 0
-    for (step_true, step_pred) in zip(y_true, y_pred):
-        n_detected += count_detected(step_true, step_pred)
-        n_steps_pred += len(step_pred)
+    _check_step_list(step_list_true)
+    _check_step_list(step_list_pred)
 
-    if n_steps_pred == 0:  # no step was predicted
+    if len(step_list_pred) == 0:  # empty prediction
         return 0.0
-    return n_detected / n_steps_pred
+
+    n_correctly_predicted = 0
+    detected_index_set = set()  # set of index of detected true steps
+    for (start_pred, end_pred) in step_list_pred:
+        mid = (start_pred + end_pred) // 2
+        for (index, (start_true, end_true)) in enumerate(step_list_true):
+            if (index not in detected_index_set) and (start_true <= mid < end_true):
+                n_correctly_predicted += 1
+                detected_index_set.add(index)
+                break
+
+    return n_correctly_predicted / len(step_list_pred)
 
 
-def _step_detection_recall(y_true, y_pred):
-    """A detected step is counted as correct if the mean of its start and end times lies
-        inside an annotated step.
+def _step_detection_recall(step_list_true, step_list_pred):
+    """Recall is the number of detected annotated steps divided by the total number of annotated
+    steps. An annotated step is counted as detected if its mid-index lies inside a predicted step.
+    Note that an annotated step can only be detected once. If several annotated steps are detected
+    with the same predicted step, all but one are considered undetected.
+    Here, recall is computed on a single prediction task (all steps correspond to the same
+    signal).
+
+    The lists y_true_ and y_pred are lists of steps, for instance:
+        - step_list_true: [[357, 431], [502, 569], [633, 715], [778, 849], [907, 989]]
+        - step_list_pred: [[293, 365], [422, 508], [565, 642], [701, 789]]
 
     Arguments:
-        y_true {List} -- list of true list of steps
-        y_pred {List} -- predicted steps
+        step_list_true {List} -- list of true steps
+        step_list_pred {List} -- list of predicted steps
 
     Returns:
         float -- recall, between 0.0 and 1.0
-
     """
-    n_detected = 0
-    n_steps_true = 0
-    for (step_true, step_pred) in zip(y_true, y_pred):
-        n_detected += count_detected(step_true, step_pred)
-        n_steps_true += len(step_true)
-    return n_detected / n_steps_true
+    _check_step_list(step_list_true)
+    _check_step_list(step_list_pred)
+
+    n_detected_true = 0
+    predicted_index_set = set()  # set of indexes of predicted steps
+
+    for (start_true, end_true) in step_list_true:
+        mid = (start_true + end_true) // 2
+        for (index, (start_pred, end_pred)) in enumerate(step_list_pred):
+            if (index not in predicted_index_set) and (start_pred <= mid < end_pred):
+                n_detected_true += 1
+                predicted_index_set.add(index)
+                break
+    return n_detected_true / len(step_list_true)
 
 
 class FScoreStepDetection(BaseScoreType):
@@ -231,7 +227,9 @@ class FScoreStepDetection(BaseScoreType):
         self.precision = precision
 
     def __call__(self, y_true, y_pred) -> float:
-        """Geometric between precision and recall.
+        """
+        Calculate f-score (geometric mean between precision and recall) for each instance (each
+        signal) and return the weighted average over instances.
 
         The lists y_true_ and y_pred are lists of lists of steps, for instance:
             - y_true: [[[907, 989]] [[357, 431], [502, 569]], [[633, 715], [778, 849]]]
@@ -244,12 +242,17 @@ class FScoreStepDetection(BaseScoreType):
         Returns:
             float -- f-score, between 0.0 and 1.0
         """
-        prec = _step_detection_precision(y_true, y_pred)
-        rec = _step_detection_recall(y_true, y_pred)
+        fscore_list = list()
 
-        if prec + rec < 1e-6:
-            return 0.
-        return (2 * prec * rec) / (prec + rec)
+        for (step_list_true, step_list_pred) in zip(y_true, y_pred):
+            prec = _step_detection_precision(step_list_true, step_list_pred)
+            rec = _step_detection_recall(step_list_true, step_list_pred)
+            if prec + rec < 1e-6:
+                fscore_list.append(0.0)
+            else:
+                fscore_list.append((2 * prec * rec) / (prec + rec))
+
+        return np.mean(fscore_list)
 
 # --------------------------------------
 # 3) Prediction types